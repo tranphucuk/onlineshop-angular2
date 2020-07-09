06dc4214a310d64008df487c367ebe809a79733c
TRAN <p.n.tran999@gmail.com>
2020-07-10 02:06:09 +0700

    commit lesson 13: buil home page

--- src/app/main/home/home.module.ts
+++ src/app/main/home/home.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
+import {Routes, RouterModule} from '@angular/router';
 
+export const homeRoutes: Routes =[
+  {path:'', component:HomeComponent}
+]
 
 
 @NgModule({
   declarations: [HomeComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(homeRoutes)
   ]
 })
 export class HomeModule { }
--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -4,12 +4,14 @@ import { MainComponent } from './main.component';
 import {mainRoutes} from './main.routes';
 import {Routes,RouterModule} from '@angular/router';
 import {UserModule} from './user/user.module'
+import {HomeModule} from './home/home.module'
 
 @NgModule({
   declarations: [MainComponent],
   imports: [
     CommonModule,
     UserModule,
+    HomeModule,
     RouterModule.forChild(mainRoutes)
   ]
 })
